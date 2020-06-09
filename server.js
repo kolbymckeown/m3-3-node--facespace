3c10c0b36f9ac6b017a7b23515ef60cd36aeccaf
kolbymckeown <mckeownkolby@gmail.com>
2020-06-09 15:27:54 -0400

    Minimally Completed (75%)

--- server.js
+++ server.js
@@ -1,50 +1,73 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const { users } = require('./data/users');
+const { users } = require("./data/users");
 
 let currentUser = {};
 
+const getFriends = (arr) => {
+	return users.filter((user) => arr.includes(user._id));
+};
+
 // declare the 404 function
 const handleFourOhFour = (req, res) => {
-  res.status(404).send("I couldn't find what you're looking for.");
+	res.status(404).send("I couldn't find what you're looking for.");
 };
 
 const handleHomepage = (req, res) => {
-  res.status(200).render('pages/homepage', { users: users });
+	res.status(200).render("pages/homepage", { users: users });
 };
 
 const handleProfilePage = (req, res) => {
-  const userId = req.params._id
-  const user = users.find((elem) => {
-    if (elem._id === userId) return true;
-  })
-  if (user) {
-  res.status(200).render(`pages/profile`, { 
-    title: user,
-    user: user }) // outputting blank page (Not showing users.name)
+	const userId = req.params._id;
+	const user = users.find((elem) => {
+		if (elem._id === userId) return true;
+	});
+	if (user) {
+		res.status(200).render(`pages/profile`, {
+			title: user,
+			user: user,
+			friends: getFriends(user.friends),
+		});
+	}
+};
 
+const handleSignin = (req, res) => {
+  res.status(200).render('pages/signin');
 }
 
+const handleName = (req, res) => {
+  const firstName = req.query.firstName;
+  const user = users.find((elem) => {
+    if (elem._id === firstName || elem.name === firstName) return true;
+  });
+
+  if (user) {
+    currentUser = user;
+    res.status(200).redirect(`users/${user._id}`);
+  } else {
+    res.status(404).redirect('/signin');
+  }
 };
 
 // -----------------------------------------------------
 // server endpoints
 express()
-  .use(morgan('dev'))
-  .use(express.static('public'))
-  .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+	.use(morgan("dev"))
+	.use(express.static("public"))
+	.use(express.urlencoded({ extended: false }))
+	.set("view engine", "ejs")
 
-  // endpoints
+	// endpoints
 
-  .get('/', handleHomepage)
-  .get('/users/:_id', handleProfilePage)
+	.get("/", handleHomepage)
+	.get("/users/:_id", handleProfilePage)
+  .get("/signin", handleSignin)
+  .get('/getname', handleName)
 
+	// a catchall endpoint that will send the 404 message.
+	.get("*", handleFourOhFour)
 
-  // a catchall endpoint that will send the 404 message.
-  .get('*', handleFourOhFour)
-
-  .listen(8000, () => console.log('Listening on port 8000'));
+	.listen(8000, () => console.log("Listening on port 8000"));
